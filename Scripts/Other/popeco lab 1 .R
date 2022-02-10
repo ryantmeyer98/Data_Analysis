396567f3cf28861e800d30244c61a369596d3ba5
Ryan Meyer <RyanMeyer@MacBook-Pro.local>
2022-02-10 09:19:27 -0600

    Made cool graphs for population ecology assignment.

--- Scripts/Other/popeco lab 1 .R
+++ Scripts/Other/popeco lab 1 .R
@@ -35,15 +35,133 @@ lambda.df <- lambda.df %>%
 # put the dataframes back together
 full.df <- bind_rows(full.df, lambda.df)
 
+full.df <- full.df %>%
+  mutate(age_class = as.factor(age_class))
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # MAKE SOME PLOTS ----
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-full.df %>%
-  ggplot(aes(x = age_class, y = value, color = model, shape = metric)) +
+full.plot <- full.df %>%
+  ggplot(aes(x = age_class, y = value, color = model)) +
+  geom_line() +
   geom_point() +
   labs(x = "Age Class", y = "Value") + 
   theme(axis.line = element_line(linetype = "solid"),
     panel.background = element_rect(fill = NA),
     legend.key = element_rect(fill = NA),
     legend.background = element_rect(fill = NA))
+full.plot
+
+stable_distribution.plot <- full.df %>%
+  filter(metric == "stable_distribution") %>%
+  ggplot(aes(x = age_class, y = value, color = model)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Age Class", y = "Stable Distribution") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+
+final_distribution.plot <- full.df %>%
+  filter(metric == "final_distribution") %>%
+  ggplot(aes(x = age_class, y = value, color = model)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Age Class", y = "Final Distribution") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+reproductive_value.plot <- full.df %>%
+  filter(metric == "reproductive_value") %>%
+  ggplot(aes(x = age_class, y = value, color = model)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Age Class", y = "Reproductive Value") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+
+lambda.plot <- full.df %>%
+  filter(metric == "final_lambda") %>%
+  ggplot(aes(x = age_class, y = value, color = model)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Age Class", y = "Final Lambda") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+final_distribution.plot
+lambda.plot
+reproductive_value.plot
+stable_distribution.plot
+
+plot.plot <- final_distribution.plot + lambda.plot + 
+  reproductive_value.plot + stable_distribution.plot + plot_layout(guides = "collect")
+plot.plot
+
+full.df %>%
+  ggplot(aes(x = model, y = value, color = age_class, shape = metric)) +
+  geom_point() +
+  geom_line()
+  
+stable_distribution_model.plot <- full.df %>%
+  filter(metric == "stable_distribution") %>%
+  ggplot(aes(x = model, y = value, color = age_class, shape = metric)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Model", y = "Value") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+final_distribution_model.plot <- full.df %>%
+  filter(metric == "final_distribution") %>%
+  ggplot(aes(x = model, y = value, color = age_class, shape = metric)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Model", y = "Value") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+reproductive_value_model.plot <- full.df %>%
+  filter(metric == "reproductive_value") %>%
+  ggplot(aes(x = model, y = value, color = age_class, shape = metric)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Model", y = "Value") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+lambda_model.plot <- full.df %>%
+  filter(metric == "final_lambda") %>%
+  ggplot(aes(x = model, y = value, color = age_class, shape = metric)) +
+  geom_line() +
+  geom_point() +
+  labs(x = "Model", y = "Value") + 
+  theme(axis.line = element_line(linetype = "solid"),
+        panel.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        legend.background = element_rect(fill = NA))
+
+stable_distribution_model.plot + final_distribution_model.plot +
+reproductive_value_model.plot + lambda_model.plot
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# MAKE WIDE AND DO PLOTS ----
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# make wide
+
