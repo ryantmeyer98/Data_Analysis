1074913812921ed419ce0a21936c17fc9ed24bc9
Ryan Meyer <RyanMeyer@MacBook-Pro.local>
2022-02-15 18:58:20 -0600

    Manova and graphing.

--- Scripts/Other/popeco lab 2.R
+++ Scripts/Other/popeco lab 2.R
@@ -7,6 +7,8 @@ library(skimr)
 library(janitor)
 library(patchwork)
 library(dplyr)
+library(car)
+library(skimr)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # READ IN DATA ----
@@ -150,14 +152,38 @@ r0.plot + hours.plot + final_percent_infected.plot +
 peak_infection_rate.plot + peak_recovery_rate.plot + 
 still_susceptible.plot + plot_layout(guides = "collect")
 
+# facet wrap
+
+full.df %>%
+  ggplot(aes(x = community, y = value)) +
+  stat_summary(
+    fun = mean, na.rm = TRUE, geom = "point", size = 4,
+    position = position_dodge(width = 0.3)) +
+  stat_summary(
+    fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
+    position = position_dodge(width = 0.3)) + 
+  facet_wrap(~metric) +
+  theme_light()
+
+
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # ANALYSIS ----
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # doing a manova for this
-full.aov <- manova(cbind(hours, R0) ~ community, data = full.df)
+# pivot to wide
 
+wide.df <- full.df %>%
+  pivot_wider(names_from = "metric", values_from = "value")
 
+full.aov <- lm(cbind(hours, R0, final_percent_infected,
+                         final_percent_recovered, peak_infection_rate, 
+                         peak_recovery_rate, still_susceptible) ~ community, data = wide.df)
+summary.aov(full.aov)
+Manova(full.aov, test.statistic = "Pillai")
+Manova(full.aov, type = "3")
 
-
+wide.df %>%
+  filter(community == "baseline") %>%
+  skim()
 
