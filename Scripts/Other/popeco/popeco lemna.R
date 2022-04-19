a7d844841389a1a33237b376407912ab6920b4ec
Ryan Meyer <RyanMeyer@MacBook-Pro.local>
2022-04-19 11:24:07 -0500

    File management

--- Scripts/Other/popeco/popeco lemna.R
+++ Scripts/Other/popeco/popeco lemna.R
@@ -14,6 +14,9 @@ library(car)
 library(lme4)
 library(lmerTest)
 library(nlme)
+library(knitr)
+library(emmeans)
+library(multcomp)
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # THEME ----
@@ -73,41 +76,57 @@ wide.df <- wide.df %>%
 wide.df <- wide.df %>%
   mutate(r0 = lx * births)
 
-# # average table
-# table.df <- wide.df %>%
-#   group_by(trt) %>%
-#   summarize(avg_n = mean(n),
-#             avg_births = mean(births),
-#             avd_deaths = mean(deaths),
-#             avg_r = mean(r))
-# 
-# # convert wide to long
-# long.df <- wide.df %>%
-#   pivot_longer(cols = c(n, births, deaths),
-#               names_to = "metric",
-#               values_to = "value")
+# r test
+wide.df <- wide.df %>%
+  mutate(tc = xlxbx / lxbx) %>%
+  mutate(r = log(r0) / tc) %>%
+  mutate(lambda = exp(r))
+
+# average table
+table.df <- wide.df %>%
+  group_by(trt) %>%
+  summarize(avg_n = mean(n),
+            avg_births = mean(births),
+            avg_deaths = mean(deaths),
+            avg_r = mean(r, na.rm = TRUE),
+            avg_r0 = mean(r0),
+            avg_lambda = mean(lambda, na.rm = TRUE))
+
+table_two.df <- wide.df %>%
+  group_by(t) %>%
+  filter(t != 0) %>%
+  summarize(avg_n = mean(n),
+            avg_births = mean(births),
+            avg_deaths = mean(deaths),
+            avg_r = mean(r, na.rm = TRUE),
+            avg_r0 = mean(r0),
+            avg_lambda = mean(lambda, na.rm = TRUE))
+
+table.df <- table.df %>%
+  rename(average_population_size = avg_n) %>%
+  rename(average_births = avg_births) %>%
+  rename(average_r = avg_r) %>%
+  rename(average_R0 = avg_r0) %>%
+  rename(treatment = trt)
+
+library(gridExtra)
+grid.table(table_two.df)
+table.df
+
+# convert wide to long
+long.df <- wide.df %>%
+  pivot_longer(cols = c(n, births, deaths),
+              names_to = "metric",
+              values_to = "value")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # PLOTTING ----
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-# all figures
-r_by_t.plot
-r_by_trt.plot
-r0_t.plot
-r0_trt.plot
-n_t.plot
-n_trt.plot
-
-
-r_by_t.plot + r_by_trt.plot
-r0_t.plot + r0_trt.plot
-n_t.plot + n_trt.plot
-
-# r by treatment for t = 1
-r_by_trt.plot <- wide.df %>%
+# final plots
+final1.plot <- wide.df %>%
   filter(t !=0) %>%
-  ggplot(mapping = aes(x = trt, y = r, color = t)) +
+  ggplot(mapping = aes(x = trt, y = lambda)) +
   geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
   stat_summary(
     fun = mean, na.rm = TRUE, geom = "point", size = 5,
@@ -115,14 +134,14 @@ r_by_trt.plot <- wide.df %>%
   stat_summary(
     fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
     position = position_dodge(width = 0.3)) +
-  theme_isco() +
-  labs(x = "Potassium Phosphate (mg/L)")
-r_no_t0.plot
+  facet_wrap(~t) + 
+  labs(x = "Added Potassium Phosphate (mg/L)", y = "Lambda") +
+  theme_light()
+final1.plot
 
-# r by time with treatment as a color
-r_by_t.plot <- wide.df %>%
-  filter(t != 0) %>%
-  ggplot(mapping = aes(x = t, y = r, color = trt)) +
+final2.plot <- wide.df %>%
+  filter(t !=0) %>%
+  ggplot(mapping = aes(x = trt, y = r0)) +
   geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
   stat_summary(
     fun = mean, na.rm = TRUE, geom = "point", size = 5,
@@ -130,13 +149,13 @@ r_by_t.plot <- wide.df %>%
   stat_summary(
     fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
     position = position_dodge(width = 0.3)) +
-  theme_isco()
-r_by_t.plot  
+  labs(x = "Added Potassium Phosphate (mg/L)", y= "Net Reproductive Rate R0") +
+  facet_wrap(~t) + 
+  theme_light()
 
-# reproductive rate plot
-r0_t.plot <- wide.df %>%
-  filter(t != 0) %>%
-  ggplot(aes(x = t, y = r0, color = trt)) +
+final3.plot <- wide.df %>%
+  filter(t !=0) %>%
+  ggplot(mapping = aes(x = t, y = lambda)) +
   geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
   stat_summary(
     fun = mean, na.rm = TRUE, geom = "point", size = 5,
@@ -144,12 +163,13 @@ r0_t.plot <- wide.df %>%
   stat_summary(
     fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
     position = position_dodge(width = 0.3)) +
-  theme_isco()
-r0.plot
-  
-r0_trt.plot <- wide.df %>%
-  filter(t != 0) %>%
-  ggplot(aes(x = trt, y = r0, color = t)) +
+  facet_wrap(~trt) + 
+  labs(x = "Time", y = "Lambda") +
+  theme_light()
+
+final4.plot <- wide.df %>%
+  filter(t !=0) %>%
+  ggplot(mapping = aes(x = t, y = r0)) +
   geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
   stat_summary(
     fun = mean, na.rm = TRUE, geom = "point", size = 5,
@@ -157,51 +177,33 @@ r0_trt.plot <- wide.df %>%
   stat_summary(
     fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
     position = position_dodge(width = 0.3)) +
-  theme_isco()
-r0_trt.plot
+  labs(x = "Time", y= "Net Reproductive Rate R0") +
+  facet_wrap(~trt) + 
+  theme_light()
 
-
-# what about n
-n_t.plot <- wide.df %>%
-  filter(t != 0) %>%
-  ggplot(aes(x = t, y = n, color = trt)) +
-  geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
-  stat_summary(
-    fun = mean, na.rm = TRUE, geom = "point", size = 5,
-    position = position_dodge(width = 0.3)) +
-  stat_summary(
-    fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
-    position = position_dodge(width = 0.3)) +
-  theme_isco()
-n_t.plot
-
-n_trt.plot <- wide.df %>%
-  filter(t != 0) %>%
-  ggplot(aes(x = trt, y = n, color = t)) +
-  geom_point(size = 2, position = position_dodge2(width = 0.3), shape = 5) +
-  stat_summary(
-    fun = mean, na.rm = TRUE, geom = "point", size = 5,
-    position = position_dodge(width = 0.3)) +
-  stat_summary(
-    fun.data = mean_se, na.rm = TRUE, geom = "errorbar", width = 0.5,
-    position = position_dodge(width = 0.3)) +
-  theme_isco()
-n_trt.plot
-
-
+final1.plot + final2.plot + plot_layout(ncol = 2)
+final3.plot + final4.plot
+         
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # STATS ----
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
+# does treatment effect r?
 # model 
-one.lm <- lmer(r ~ trt + t + (1|cup),
-               data = wide.df,
+one.lm <- lmer(lambda ~ trt + t + (1|cup),
+               data = test.df,
                REML = TRUE)
 Anova(one.lm, type = "3")
 ranova(one.lm)
 
+# assumptions
+residuals <- resid(one.lm)
+plot(fitted(one.lm), residuals)
+qqnorm(residuals)
+qqline(residuals)
+
 # another way
-rand.lm <- lme(r ~ trt + t, 
+rand.lm <- lme(r ~ trt + t + t * trt, 
                random = ~1|cup,
                data = wide.df,
                method = "REML")
@@ -209,6 +211,7 @@ no_rand.lm <- gls(r ~ trt + t,
                   data = wide.df, 
                   method = "REML")
 anova(rand.lm, no_rand.lm)
+Anova(rand.lm, type = "3")
 
 # both ways the random effect does not affect the model and the interaction does not affect the 
 # model
@@ -220,3 +223,31 @@ residuals <- resid(one.lm)
 plot(fitted(one.lm), residuals)
 qqnorm(residuals)
 
+# post-f tests
+one.emm <- emmeans(one.lm, pairwise ~ t, adjust = "bonferroni")
+plot(one.emm, comparisons = TRUE) + theme_classic()
+emminteraction = emmeans(one.lm, pairwise ~ t, adjust = "bonferroni", alpha = 0.5)
+emminteraction$contrasts
+cld(one.emm, Letters = letters)
+
+
+# does treatment effect r0?
+# model 
+test.df <- wide.df %>%
+  filter(t != 0)
+two.lm <- lmer(r0 ~ trt + t + (1|cup),
+               data = test.df,
+               REML = TRUE)
+Anova(two.lm, type = "3")
+ranova(two.lm)
+
+residuals <- resid(two.lm)
+plot(fitted(two.lm), residuals)
+qqnorm(residuals)
+qqline(residuals)
+
+two.emm <- emmeans(two.lm, pairwise ~ t, adjust = "bonferroni")
+plot(two.emm, comparisons = TRUE) + theme_classic()
+emminteraction = emmeans(two.lm, pairwise ~ t, adjust = "bonferroni", alpha = 0.5)
+emminteraction$contrasts
+cld(one.emm, Letters = letters)
